67096dc0d0dee98707788aaa2868e9293cc6e58a
Adam Řezníček <adam_reznicek@icloud.com>
2024-03-12 14:08:25 +0100

    refactor: simplify AbstractReader interface

--- utils/data_import/abstract/reader.py
+++ utils/data_import/abstract/reader.py
@@ -2,10 +2,9 @@ from abc import ABC, abstractmethod
 
 # typing
 from tablib import Dataset
-from django.core.files.base import File
 
 
 class AbstractReader(ABC):
     @abstractmethod
-    def read(self, file: File) -> Dataset:
+    def read(self) -> Dataset:
         ...
