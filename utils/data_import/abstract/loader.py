584060e904bed13713f30b8b3532e7131728b183
Adam Řezníček <adam_reznicek@icloud.com>
2024-03-12 14:58:05 +0100

    refactor: AbstractLoader interface

--- utils/data_import/abstract/loader.py
+++ utils/data_import/abstract/loader.py
@@ -1,10 +1,7 @@
 from abc import ABC, abstractmethod
 
-# typing
-from tablib import Dataset
-
 
 class AbstractLoader(ABC):
     @abstractmethod
-    def load(self, dataset: Dataset):
+    def load(self):
         ...
