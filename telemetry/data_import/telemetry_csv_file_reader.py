0e883d1a6e66867eb462ba10ee3dfa1902cb184d
Adam Řezníček <adam_reznicek@icloud.com>
2024-03-12 16:43:53 +0100

    refactor: TelemetryCSVFileReader now only inherits the common CSVFileReader

--- telemetry/data_import/telemetry_csv_file_reader.py
+++ telemetry/data_import/telemetry_csv_file_reader.py
@@ -1,20 +1,5 @@
-from utils.data_import.abstract import AbstractReader
-
-# exceptions
-from utils.data_import.exceptions import ReaderException
-
-# typing
-from tablib import Dataset
-from django.core.files.base import File
+from utils.data_import.readers import CSVFileReader
 
 
-class TelemetryCSVFileReader(AbstractReader):
-    def __init__(self, file: File):
-        self.path = file.path
-
-    def read(self) -> Dataset:
-        try:
-            with open(self.path, 'r') as f:
-                return Dataset().load(f, headers=True)
-        except Exception:
-            raise ReaderException()
+class TelemetryCSVFileReader(CSVFileReader):
+    ...
