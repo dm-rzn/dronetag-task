d712fe5b140ca6fe8049e73f5917380a0c607c17
Adam Řezníček <adam_reznicek@icloud.com>
2024-03-12 16:43:34 +0100

    feat: common csv -> tablib.Dataset reader

---
+++ utils/data_import/readers/__init__.py
@@ -0,0 +1,1 @@
+from .csv_file_reader import CSVFileReader  # noqa: F401
---
+++ utils/data_import/readers/csv_file_reader.py
@@ -0,0 +1,20 @@
+from utils.data_import.abstract import AbstractReader
+
+# exceptions
+from utils.data_import.exceptions import ReaderException
+
+# typing
+from tablib import Dataset
+from django.core.files.base import File
+
+
+class CSVFileReader(AbstractReader):
+    def __init__(self, file: File):
+        self.path = file.path
+
+    def read(self) -> Dataset:
+        try:
+            with open(self.path, 'r') as f:
+                return Dataset().load(f, headers=True)
+        except Exception:
+            raise ReaderException()
