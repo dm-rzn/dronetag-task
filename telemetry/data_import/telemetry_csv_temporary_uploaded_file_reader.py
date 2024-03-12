9a86cffe9ed47a292ee01313e6eb5c04d8f20c65
Adam Řezníček <adam_reznicek@icloud.com>
2024-03-12 16:46:53 +0100

    refactor: TelemetryCSVTemporaryUploadedFileReader now only inherits the common reader

--- telemetry/data_import/telemetry_csv_temporary_uploaded_file_reader.py
+++ telemetry/data_import/telemetry_csv_temporary_uploaded_file_reader.py
@@ -1,9 +1,5 @@
-from .telemetry_csv_file_reader import TelemetryCSVFileReader
-
-# typing
-from django.core.files.uploadedfile import TemporaryUploadedFile
+from utils.data_import.readers import CSVTemporaryUploadedFileReader
 
 
-class TelemetryCSVTemporaryUploadedFileReader(TelemetryCSVFileReader):
-    def __init__(self, file: TemporaryUploadedFile):
-        self.path = file.temporary_file_path()
+class TelemetryCSVTemporaryUploadedFileReader(CSVTemporaryUploadedFileReader):
+    ...
