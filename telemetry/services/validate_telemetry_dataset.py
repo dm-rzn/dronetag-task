b6d424cb23443e82b4cb595cc4b88e218ca34751
Adam Řezníček <adam_reznicek@icloud.com>
2024-03-17 16:55:08 +0100

    feat: when reader, validator or loader exception is raised, reraise it as a status exception

--- telemetry/services/validate_telemetry_dataset.py
+++ telemetry/services/validate_telemetry_dataset.py
@@ -4,6 +4,16 @@ from utils.data_import.services import validate_dataset
 # resources
 from telemetry.resources import TelemetryDatapointResource
 
+# exceptions
+from utils.data_import.exceptions import (
+    ReaderException,
+    ValidatorException,
+)
+from telemetry.exceptions import (
+    TelemetryReaderException,
+    TelemetryValidatorException,
+)
+
 # typing
 from django.core.files.base import File
 from users.models import User
@@ -11,7 +21,12 @@ from users.models import User
 
 def validate_telemetry_dataset(file: File, user: User):
     '''
-    :raises: ReaderExcpetion
-    :raises: ValidatorException
+    :raises: TelemetryReaderExcpetion
+    :raises: TelemetryValidatorException
     '''
-    validate_dataset(file, user, TelemetryDatapointResource)
+    try:
+        validate_dataset(file, user, TelemetryDatapointResource)
+    except ReaderException:
+        raise TelemetryReaderException()
+    except ValidatorException:
+        raise TelemetryValidatorException()
