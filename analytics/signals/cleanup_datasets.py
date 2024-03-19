60a8a55e9e7269c263bd0f9b99a825608900e443
Adam Řezníček <adam_reznicek@icloud.com>
2024-03-19 14:59:46 +0100

    fix: added dispatch_uid to signal

--- analytics/signals/cleanup_datasets.py
+++ analytics/signals/cleanup_datasets.py
@@ -3,7 +3,7 @@ from django.dispatch import receiver
 from analytics.models import FlightAnalyticsDataset
 
 
-@receiver(post_delete, sender=FlightAnalyticsDataset)
+@receiver(post_delete, sender=FlightAnalyticsDataset, dispatch_uid='post_delete_flight_analytics_dataset_cleanup')
 def post_delete_cleanup_handler(sender, instance, *args, **kwargs):
     instance.telemetry.delete()
     instance.statuses.delete()
