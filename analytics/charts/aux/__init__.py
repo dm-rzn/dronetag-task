7aca008ce3ad4daac9898d40a2e32aafeaa54bbd
Adam Řezníček <adam_reznicek@icloud.com>
2024-03-19 13:18:26 +0100

    refactor: move chart data preparation functions from services to aux

---
+++ analytics/charts/aux/__init__.py
@@ -0,0 +1,2 @@
+from .prepare_chart_data import prepare_chart_data  # noqa: F401
+from .prepare_chart_data_grouped import prepare_chart_data_grouped  # noqa: F401
--- analytics/services/__init__.py
+++ analytics/services/__init__.py
@@ -2,5 +2,3 @@ from .create_dataset import create_dataset  # noqa: F401
 from .order_datasets import order_datasets  # noqa: F401
 from .paginate_datasets import paginate_datasets  # noqa: F401
 from .search_datasets import search_datasets  # noqa: F401
-from .prepare_chart_data import prepare_chart_data  # noqa: F401
-from .prepare_chart_data_grouped import prepare_chart_data_grouped  # noqa: F401
